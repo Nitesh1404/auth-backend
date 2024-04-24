5e9f764b01271c26ceea5482ebded196dee6662c
Nitesh Kanojia <niteshkanojia1010@gmail.com>
2024-04-24 08:41:05 +0530

    cors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const connectToMongoDb = require('./db');
 require('dotenv').config();
 
+
+
 var cors = require('cors')
 // connection to mongodb
 connectToMongoDb();
@@ -9,10 +11,19 @@ const app = express();
 const port = 5000;
 
 // cors middleware
-app.use(cors())
+const conrsConfig = {
+	origin: '*',
+	credential: true,
+	methods: ["POST", "GET"],
+}
+app.use(cors(conrsConfig));
 
 // middleware to fetch json data from the website body
 app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send("app running");
+})
 
 app.use('/api/auth', require('./routes/auth'));
 
